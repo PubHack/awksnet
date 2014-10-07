c59d0b9b38bf1b74fb9b869a06b7f7624b25e324
CraigChilds94 <childscraig17@gmail.com>
2014-10-07 13:18:00 +0100

    Add two seeded users

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -14,18 +14,32 @@ class DatabaseSeeder extends Seeder {
 		DB::table('users')->delete();
 		DB::table('situations')->delete();
 
-		$user = User::create(array(
-			'username' 	=> 'user',
-			'email' 	=> 'childscraig17@gmail.com',
-			'password' 	=> Hash::make('hello'),
-			'city'		=> 'Littlehampton'
+		$jake = User::create(array(
+			'username' 	=> 'jake',
+			'email' 	=> 'shout@cleary.net',
+			'password' 	=> Hash::make('jake'),
+			'city'		=> 'Worthing'
 		));
 
 		$situation = Situation::create(array(
 			'body' 		=> 'I am making a statement here',
-			'upvotes' 	=> 10,
+			'upvotes' 	=> 1,
+			'downvotes' => 20000,
+			'user_id' 	=> $jake->id
+		));
+
+		$craig = User::create(array(
+			'username' 	=> 'craig',
+			'email' 	=> 'childscraig17@gmail.com',
+			'password' 	=> Hash::make('craig'),
+			'city'		=> 'Littlehampton'
+		));
+
+		$situation = Situation::create(array(
+			'body' 		=> 'I am making a better statement here',
+			'upvotes' 	=> 10113,
 			'downvotes' => 2,
-			'user_id' 	=> $user->id
+			'user_id' 	=> $craig->id
 		));
 	}
 
