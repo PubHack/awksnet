d5902a2a21671584f7b2005f72759b5a4df23dc4
clear-y <shout@jakecleary.net>
2014-10-07 13:28:37 +0100

    Add fields for login

--- app/views/auth/login.blade.php
+++ app/views/auth/login.blade.php
@@ -2,4 +2,20 @@
 
 @section('content')
 
+    {{ Form::open(['url' => '/']) }}
+
+        <div class="form__feild-set">
+            {{ Form::label('username', 'Username') }}
+            {{ Form::text('username', Input::old('username'), ['placeholder'=>'awkwardturtle69']) }}
+        </div>
+
+        <div class="form__feild-set">
+            {{ Form::label('password', 'Password') }}
+            {{ Form::password('password') }}
+        </div>
+
+        {{ Form::submit('Login') }}
+
+    {{ Form::close() }}
+
 @stop
