bb1d47b065b8f2e1c526ee542f6eff58b83ebe0b
clear-y <shout@jakecleary.net>
2014-10-07 10:43:14 +0100

    Add initial template scaffolding

---
+++ app/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+        </div>
+        <div class="page__footer">
+            <p>awksnet - Where awkwardness is celebrated.</p>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    </body>
+</html>
