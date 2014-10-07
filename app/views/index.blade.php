7d7f5729fffd2d59e21832d315d06053dca080d9
clear-y <shout@jakecleary.net>
2014-10-07 13:55:47 +0100

    Added basic feed template

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -2,4 +2,29 @@
 
 @section('content')
 
+    <div class="feed">
+
+        @forelse($situations as $situation)
+            <article class="item">
+                <header class="item__header">
+                    <h2 class="item__heading">{{ $situation->user->username }}</h2>
+                    <ul class="item__meta">
+                        <li>{{ $situation->created_at }}</li>
+                        <li>{{ $situation->upvotes }}</li>
+                        <li>{{ $situation->downvotes }}</li>
+                    </ul>
+                </header>
+                <div class="item__content">
+                    {{ $situation->body }}
+                </div>
+            </article>
+        @empty()
+            <div class="message--warning">
+                <h2>Well, this is awkward...</h2>
+                <p>No one's posted anything yet.</p>
+            </div>
+        @endforelse()
+
+    </div>
+
 @stop
