cb20db4d56d9f8f97ce62a7de9fbef637aec7b5b
CraigChilds94 <childscraig17@gmail.com>
2014-10-07 12:56:15 +0100

    Revert env stuff

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => getenv('DB_HOST'),
-			'database'  => getenv('DB_NAME'),
-			'username'  => getenv('DB_USER'),
-			'password'  => getenv('DB_PASS'),
+			'host'      => '127.0.0.1',
+			'database'  => 'awksnet',
+			'username'  => 'root',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
